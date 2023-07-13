96695cbde77ede76bd9e8f06e18da12c57ec3ee9
Aishah <maishah2540@gmail.com>
2023-07-13 18:29:37 +0000

    Correcting task 0.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
 
     a, b = 1, 2
-    print("{:d} + {:d} = {:d}" .format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
