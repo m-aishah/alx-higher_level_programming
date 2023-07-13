1829a261c9d5437b899c3c178b41c78576bcc72f
Aishah <maishah2540@gmail.com>
2023-07-13 21:15:35 +0000

    Answered task 6- a program that imports all functions from the file calculator_1.py and handles basic operations- using match which can only be run on versions of python after python 3.10

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import exit, argv
+
+    number_of_arguments = len(argv) - 1
+
+    if number_of_arguments != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, operator, b = argv[1], argv[2], argv[3]
+    match operator:
+        case '+':
+            result = int(a) + int(b)
+        case '-':
+            result = int(a) - int(b)
+        case '*':
+            result = int(a) * int(b)
+        case '/':
+            result = int(a) / int(b)
+        case '_':
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+    print("{} {} {} = {}" .format(a, operator, b, result))
+    exit(0)
