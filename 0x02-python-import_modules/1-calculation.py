a0a1cc208f648683e170d6e880e2ce7d3aac531a
Aishah <maishah2540@gmail.com>
2023-07-13 18:56:39 +0000

    Correcting code for task 1.'

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    """Does some math using functions in calculator_1.py
     
-    add - Adds 2 integers
-    sub - Subtracts 2 integers
-    mul - Multiplies 2 integers
-    div - Divides 2 integers
-    """
     a = 10
     b = 5
-
-    print('{:d} + {:d} = {:d}' .format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}' .format(a, b, sub(a, b)))
-    print('{:d} * {:d} = {:d}' .format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:d}' .format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
