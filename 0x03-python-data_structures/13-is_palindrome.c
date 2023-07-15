396b7115e7e3aa9af2f1567a2e959500e31b6973
Aishah <maishah2540@gmail.com>
2023-07-15 18:49:35 +0000

    Task 13. Trying to fix betty error

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,31 +5,30 @@
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *traverse;
-    char forward[1024], backward[1024];
-    int index = 0;
+	listint_t *traverse;
+	char forward[1024], backward[1024];
+	int index = 0;
 
-    if (*head == NULL)
-        return (1);
-    traverse = *head;
-if (traverse-> next == NULL)
-        return (1);    
-while (traverse->next != NULL)
-    {
-        forward[index] = traverse->n;
-        index++;
-        traverse = traverse->next;
-    }
-    forward[index] = traverse->n;
-    forward[index + 1] = '\0';
-    for (index = 0; forward[index]; index++)
-    {
-        backward[index] = forward[strlen(forward) - index - 1];
-    }
-    backward[index] = '\0';
-
-    if (!(strcmp(forward, backward)))
-        return (1);
-    else
-        return (0);
+	if (*head == NULL)
+		return (1);
+	
+	traverse = *head;
+	while (traverse->next != NULL)
+	{
+		forward[index] = traverse->n;
+		index++;
+		traverse = traverse->next;
+	}
+	forward[index] = traverse->n;
+	forward[index + 1] = '\0';
+	for (index = 0; forward[index]; index++)
+	{
+		backward[index] = forward[strlen(forward) - index - 1];
+	}
+	backward[index] = '\0';
+    
+	if (!(strcmp(forward, backward)))
+		return (1);
+	else
+		return (0);
 }
