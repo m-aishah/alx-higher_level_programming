29024be41055ac067d2156af4e6e92f773cdeabd
Aishah <maishah2540@gmail.com>
2023-07-15 15:52:53 +0000

    Task 6. wrote a function that prints a matrix of integers. (Fixed my code to cater for other edge cases)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    no_of_rows = len(matrix[:])
-    no_of_columns = len(matrix[0][:])
-
-    for i in range(no_of_rows):
-        for integer in matrix[i]:
-            print("{:d}".format(integer), end=" ")
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print("{:d}".format(column), end="")
+            else:
+                print("{:d}".format(column), end=" ")
         print()
