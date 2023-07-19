691a81937a2bc353488a91796375fbb7920c3d55
Aishah <maishah2540@gmail.com>
2023-07-19 23:23:55 +0000

    Stil trying to resolve a pep8 issue

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string == 'None'):
         return 0
     roman_numerals = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
     }
     output = 0
 
@@ -22,7 +22,7 @@ def roman_to_int(roman_string):
         if (index != (len(roman_string) - 1) and
                 (roman_numerals[roman_string[index]] < roman_numerals[roman_string[index + 1]])):
 
-            output += roman_numerals[character] * -1
+                output += roman_numerals[character] * -1
 
         else:
             output += roman_numerals[character]
