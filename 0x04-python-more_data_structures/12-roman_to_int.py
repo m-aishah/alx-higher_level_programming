4605be18c6b0a08a725d5d2bb7cd61db00071547
Aishah <maishah2540@gmail.com>
2023-07-19 23:56:54 +0000

    Stil trying to resove the pep8 error for task 12.

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,17 @@
 def roman_to_int(roman_string):
     """Converts a Roman Numeral to an integer"""
 
-    if (not isinstance(roman_string, str) or roman_string == 'None'):
+    if (not isinstance(roman_string, str) 
+        or roman_string == 'None'):
         return 0
     roman_numerals = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
     }
     output = 0
 
@@ -20,9 +21,9 @@ def roman_to_int(roman_string):
             return 0
 
         if (index != (len(roman_string) - 1) and
-                (roman_numerals[roman_string[index]] < roman_numerals[roman_string[index + 1]])):
-
-                output += roman_numerals[character] * -1
+                (roman_numerals[roman_string[index]]
+                 < roman_numerals[roman_string[index + 1]])):
+            output += roman_numerals[character] * -1
 
         else:
             output += roman_numerals[character]
