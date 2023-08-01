ac4d568b3275cde6ce5dfd69264e517b7cb2b26e
Aishah <maishah2540@gmail.com>
2023-08-01 16:24:24 -0700

    Task 10. correction

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """MagicClass class gotten from interpreting bytecodes provided by ALX"""
 
-
 import math
 
 
 class MagicClass:
-    """Defining the MagicClass class."""
+    """MagicClass - Represent a circle."""
 
     def __init__(self, radius=0):
-        """initializes new instance of the MagicClass.
+        """Initializes a new instance of the MagicClass.
 
         Arg:
-            radius (int): radius of the current instance of the class.
+            radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError('radius must be a number')
+            raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """Returns the area of the MagicClass."""
-        return(self.__radius ** 2 * math.pi)
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """Returns the circumference of the MagicClass."""
-        return(2 * math.pi * self.__radius)
+        return (2 * math.pi * self.__radius)
