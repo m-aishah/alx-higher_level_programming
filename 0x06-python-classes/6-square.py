35e3fbce3a499acc68dd98be589ce3d949d5d4ec
Aishah <maishah2540@gmail.com>
2023-08-01 12:09:36 -0700

    Trying to fix checker : (

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,12 +34,12 @@ class Square:
     @property
     def position(self):
         """Get the current position of the square"""
-        return self.__position
+        return (self.__position)
 
     @position.setter
     def position(self, value):
         """Set te position of the square"""
-        if (not(isinstance(value, tuple)) or
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
@@ -58,6 +58,6 @@ class Square:
 
         [print() for i in range(self.__position[1])]
         for i in range(0, self.__size):
-            [print(' ', end="") for k in range(self.__position[0])]
-            [print('#', end="") for j in range(self.__size)]
+            [print(' ', end="") for j in range(self.__position[0])]
+            [print('#', end="") for k in range(self.__size)]
             print()
