e8d1486a2f13aa91d45ed10669929f6ff5c0d578
Aishah <maishah2540@gmail.com>
2023-08-01 16:07:34 -0700

    Task 10.

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""MagicClass class gotten from interpreting bytecodes provided by ALX"""
+
+import math
+
+
+class MagicClass:
+    """Defining the MagicClass class."""
+
+    def __init__(self, radius=0):
+        """initializes new instance of the MagicClass.
+
+        Arg:
+            radius (int): radius of the current instance of the class.
+        """
+        if not isinstance(radius, int):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """returns the area of the MagicClass."""
+        return(self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """Returns the circumference of the MagicClass."""
+        return(2 * math.pi * self.__radius)
