c447448168c14a916587680741db4465763733f2
Aishah <maishah2540@gmail.com>
2023-08-01 08:27:12 -0700

    Task 2.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defining the class called Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size = 0):
+        """Method to initialize a new instance of Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
