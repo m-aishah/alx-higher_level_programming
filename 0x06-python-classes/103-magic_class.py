f0bea5f39a0b5be5f8f201911941d6014f3ab849
Aishah <maishah2540@gmail.com>
2023-08-01 16:17:41 -0700

    Task 10 - correction

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,12 +14,13 @@ class MagicClass:
         Arg:
             radius (int): radius of the current instance of the class.
         """
-        if not isinstance(radius, int) and not isinstance(radius, float):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """returns the area of the MagicClass."""
+        """Returns the area of the MagicClass."""
         return(self.__radius ** 2 * math.pi)
 
     def circumference(self):
