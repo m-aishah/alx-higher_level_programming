2134c40e49c3b151ca7b330a563cf9897bf6a68b
Aishah <maishah2540@gmail.com>
2023-08-14 11:37:51 -0700

    Changed my implementation (using the super() method) for 8-rectangle.py and 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,9 +12,9 @@ class Rectangle(BaseGeometry):
             width (int): The width of the rectangle.
             height (int): The height of te rectangle.
         """
-        self.integer_validator("width", width)
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
