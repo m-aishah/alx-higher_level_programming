7d674a93cf387fa62bcf8f57fe687df34422e82f
Aishah <maishah2540@gmail.com>
2023-08-15 13:30:05 -0700

    Task 10.

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize an instance of Student.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of Student.
+
+        If attrs is a list of strings, only attribute names 
+        contained in this list must be retrieved.
+
+        Args:
+            attrs (list): The attributes to retrieve."""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
