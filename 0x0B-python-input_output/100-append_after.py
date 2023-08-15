9826e980de6b5c2f5f4b29b6650439eb22850835
Aishah <maishah2540@gmail.com>
2023-08-15 14:03:05 -0700

    Task 13.

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for.
+        new_string (str): The string to be inserted.
+    """
+    text = ""
+    with open(filename) as f_r:
+        for line in f_r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as f_w:
+        f_w.write(text)
