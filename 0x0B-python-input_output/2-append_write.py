db675cd27356552e2c2265202424f58f506bd25c
Aishah <maishah2540@gmail.com>
2023-08-15 10:08:37 -0700

    Task 2.

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contain a function that appends to the of a text file."""
+
+
+def append_write(filename="", text=""):
+    """Append a string to the end of a text file.
+
+    Args:
+        filename (str): The name of the file to append.
+        text (str): The string to append at the end of the file.
+    Returns:
+        the number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as f:
+        return f.write(text)
