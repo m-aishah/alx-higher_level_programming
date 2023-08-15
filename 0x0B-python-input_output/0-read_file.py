5fd80d2cf69240d9bd1a6ad80d4dc3c93a7becd6
Aishah <maishah2540@gmail.com>
2023-08-15 09:49:30 -0700

    Task 0.

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contain function that reads a text file."""
+
+
+def read_file(filename=""):
+    """Read a text file and print it to stdout."""
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
