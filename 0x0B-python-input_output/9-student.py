6adb4e84f45498d61a9c2775e971f4cd0039537b
Aishah <maishah2540@gmail.com>
2023-08-15 12:19:09 -0700

    Task 9.

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+class_to_json = __import__('8-class_to_json').class_to_json
+
+
+class Student:
+    """A class representing a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize an instance of Student.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """Retrieve a dictionary representation of Student."""
+            return class_to_json(self)
