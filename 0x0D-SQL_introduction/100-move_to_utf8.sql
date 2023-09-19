e46eb72c4afb83cac7b50d26eb467bab8b490bb1
Aishah <maishah2540@gmail.com>
2023-09-19 14:19:34 -0700

    Task 17.

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Converts hbtn_0c_0 databases to UTF8. It converts the following:
+-- Database hbtn_0c_0
+-- Table first_table
+-- Field name in first_table
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
