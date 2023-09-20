b484f336e1ee12961abe2f20c04dd47923baf688
Aishah <maishah2540@gmail.com>
2023-09-20 14:00:43 -0700

    Changed the line styling and answered task 3.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- Creates the MySQL server user user_0d_1 and grants all privileges.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 
+	IF NOT EXISTS 'user_0d_1'@'localhost' 
+	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL
-ON *.*
-TO 'user_0d_1'@'localhost'
+	ON *.*
+	TO 'user_0d_1'@'localhost'
