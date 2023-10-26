01a24bfec2dc3c4ad896b17c450efd9fc2f0ff43
Aishah <maishah2540@gmail.com>
2023-10-26 13:34:12 -0700

    Tsk 5.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Lists all cities from the database hbtn_0e_4_usa
+    Usage: ./4-cities_by_state.py <mysql username>
+                                  <mysql password>
+                                  <databse name>
+                                  <city name searched>
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host="localhost",
+            port=3306
+            )
+    cur = db.cursor()
+    cur.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name` \
+                FROM `cities` \
+                INNER JOIN `states` \
+                ON `cities`.`state_id` = `states`.`id` \
+                ORDER BY `id`")
+    [print(city) for city in cur.fetchall()]
