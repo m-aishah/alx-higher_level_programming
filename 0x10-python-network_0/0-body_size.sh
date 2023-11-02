1e54cce3d76ebd1fd5162f02d4781dd5dd038885
Aishah <maishah2540@gmail.com>
2023-11-02 13:58:52 -0700

    Fix task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
- #!/bin/bash
- # Takes in a URL, sends a request to that URL, and displays the size of the body of the response.
- curl -s "$1" | wc -c
+#!/bin/bash 
+# Displays the size of the body of the response of a URL request.
+curl -s "$1" | wc -c
