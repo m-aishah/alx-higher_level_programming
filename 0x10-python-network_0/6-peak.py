d0ec45cffeb428f539d7e2d5f4a176a694b864bd
Aishah <maishah2540@gmail.com>
2023-11-25 08:11:02 -0800

    Fixed pycodestyle error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Finds the peak in a list of integers.'''
 
+
 def find_peak_recursive(list_of_integers, left_index, right_index):
     '''Return the peak number in the list of integers.'''
     if left_index == right_index:
@@ -9,9 +10,11 @@ def find_peak_recursive(list_of_integers, left_index, right_index):
     mid_index = (left_index + right_index) // 2
 
     if list_of_integers[mid_index] < list_of_integers[mid_index + 1]:
-        return find_peak_recursive(list_of_integers, mid_index + 1, right_index)
+        return find_peak_recursive(list_of_integers,
+                                   mid_index + 1, right_index)
     else:
         return find_peak_recursive(list_of_integers, left_index, mid_index)
+
 
 def find_peak(list_of_integers):
     '''Return the peak number in the list of integers.'''
