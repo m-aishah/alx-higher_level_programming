c65e63a92ba5204ebe9a8efac7be17bfcbd3c59c
Aishah <maishah2540@gmail.com>
2023-11-28 08:49:11 -0800

    Task 0.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
         web_page = response.read()
-        print("Body response")
+        print("Body response:")
         print("\t - type: {}".format(type(web_page)))
         print("\t - content: {}".format(web_page))
         print("\t - utf8 content: {}".format(web_page.decode("utf-8")))
